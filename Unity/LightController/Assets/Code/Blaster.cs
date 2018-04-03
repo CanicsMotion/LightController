bc77cd4f714c375a50b21f981134218b91f370c9
Janek <janekx21@gmail.com>
2018-04-03 15:46:53 +0200

    Some Meta and Tests

---
+++ Unity/LightController/Assets/Code/Blaster.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaster : MonoBehaviour {
+    [SerializeField] BlasterObject preset;
+    public BlasterObject local;
+
+    public Transform repr;
+    public new Light light;
+    [System.Serializable]
+    public class RotLink {
+        public enum axle {
+            X,Y,Z
+        }
+        public Transform transform;
+        public float rotation = 0;
+        public float begin = 0;
+        public float target = 0;
+        public float lastBegin = 0;
+        public axle ax = axle.X;
+    }
+    public RotLink[] links;
+    public AnimationCurve curve;
+
+    void Start () {
+        setLocal(preset);//copiere das Prefap auf die localen Variabeln
+    }
+
+    public void Rotate(float[] angles) {
+        for (int i = 0; i < links.Length; i++) {
+            try {
+                links[i].target = angles[i];
+            }
+            catch { }
+
+            links[i].begin = links[i].rotation;
+            links[i].lastBegin = 0f;
+        }
+    }
+
+    public void Look(Vector3 pos) {
+        var q = Quaternion.LookRotation(pos-transform.position, Vector3.up);
+        var a = q.eulerAngles;
+        float[] f = {a.x,a.y,a.z };
+        Rotate(f);
+    }
+
+    void setLocal(BlasterObject obj) {
+        local = preset;
+        if (local.shape != null)
+            light.cookie = local.shape.texture;
+        light.color = local.colors.Evaluate(0f);
+    }
+	
+
+	void Update () {
+        for (int i = 0; i < links.Length; i++) {
+
+            links[i].lastBegin = Mathf.MoveTowards(links[i].lastBegin, 1f, Time.deltaTime);
+
+            links[i].rotation = Mathf.Lerp(links[i].begin, links[i].target, curve.Evaluate(links[i].lastBegin));
+
+            Quaternion q = Quaternion.identity;
+            if (links[i] != null) {
+                switch (links[i].ax) {
+                    case RotLink.axle.X: q= Quaternion.Euler(links[i].rotation,0 , 0);break;
+                    case RotLink.axle.Y: q = Quaternion.Euler(0, links[i].rotation, 0); break;
+                    case RotLink.axle.Z: q = Quaternion.Euler(0, 0,links[i].rotation); break;
+                }
+                links[i].transform.localRotation = q;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            try {
+                float[] f = new float[2];
+                f[0] = links[0].rotation + 45;
+                f[1] = 45 - links[1].rotation;
+                Rotate(f);
+            }
+            catch(System.Exception e){
+                Debug.LogWarning(e.ToString());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.M)) {
+            Look(GameObject.Find("Target").transform.position);
+        }
+    }
+}
---
+++ Unity/LightController/Assets/Code/BlasterObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum blasterMode{
+    Spoter,
+
+}
+
+[CreateAssetMenu(fileName ="new Blaster",menuName ="Blaster",order =50)]
+public class BlasterObject : ScriptableObject {
+    public string Name = "Blaster Name";
+    public string ModelCode = "M0D3L";
+    public Mesh mesh = null;
+    public blasterMode mode = blasterMode.Spoter;
+    public Gradient colors = new Gradient();
+    public Sprite shape = null;
+}
