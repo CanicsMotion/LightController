f093604ce4436a4b86767131bd9291ff88cf27f7
Janek <janekx21@gmail.com>
2018-04-07 01:56:20 +0200

    new moving head ajustments

--- Unity/LightController/Assets/Code/Blaster.cs
+++ Unity/LightController/Assets/Code/Blaster.cs
@@ -19,6 +19,7 @@ public class Blaster : MonoBehaviour {
         public float target;
         public float lastBegin;
         public axle ax;
+        public bool wrap;
     }
     public RotLink[] links;
     public AnimationCurve curve;
@@ -30,7 +31,11 @@ public class Blaster : MonoBehaviour {
     public void Rotate(float[] angles) {
         for (int i = 0; i < links.Length; i++) {
             try {
-                links[i].target = angles[i];
+                if(links[i].wrap)
+                    links[i].target = Mathf.Repeat(angles[i],360);
+                else
+                    links[i].target = Mathf.Clamp(angles[i],-180, 180);
+
             }
             catch { }
 
@@ -40,9 +45,10 @@ public class Blaster : MonoBehaviour {
     }
 
     public void Look(Vector3 pos) {
-        var q = Quaternion.LookRotation(pos-transform.position, Vector3.up);
-        var a = q.eulerAngles;
-        float[] f = {a.x,a.y,a.z };
+        Vector3 off = pos - transform.position;
+        float x = Vector2.SignedAngle(Vector2.up, new Vector2(off.x, off.z));
+        float y = Vector2.SignedAngle(Vector2.up, new Vector2(new Vector2(off.x, off.z).magnitude, off.y));
+        float[] f = {x,y,0 };
         Rotate(f);
     }
 
@@ -58,8 +64,10 @@ public class Blaster : MonoBehaviour {
         for (int i = 0; i < links.Length; i++) {
 
             links[i].lastBegin = Mathf.MoveTowards(links[i].lastBegin, 1f, Time.deltaTime);
-
-            links[i].rotation = Mathf.Lerp(links[i].begin, links[i].target, curve.Evaluate(links[i].lastBegin));
+            if (links[i].wrap)
+                links[i].rotation = Mathf.LerpAngle(links[i].begin, links[i].target, curve.Evaluate(links[i].lastBegin));
+            else
+                links[i].rotation = Mathf.Lerp(links[i].begin, links[i].target, curve.Evaluate(links[i].lastBegin));
 
             Quaternion q = Quaternion.identity;
             if (links.Length>=i) {
@@ -73,13 +81,13 @@ public class Blaster : MonoBehaviour {
         }
         if (Input.GetKeyDown(KeyCode.Space)) {
             if (links.Length > 0) {
-                float[] f = { links[0].rotation + 45, 45 - links[1].rotation };
+                float[] f = { links[0].rotation + 90 + Random.Range(-20, 20), 45 - links[1].rotation + Random.Range(-20,20)+20};
 
                 Rotate(f);
             }
         }
-        if (Input.GetKeyDown(KeyCode.M)) {
-            Look(GameObject.Find("Target").transform.position);
+        if (TargetPlacer.instance && Input.GetMouseButton(0)) {
+            Look(TargetPlacer.instance.transform.position);
         }
     }
 }
---
+++ Unity/LightController/Assets/Code/TargetPlacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPlacer : MonoBehaviour {
+
+    public static TargetPlacer instance;
+
+    // Use this for initialization
+    void Start () {
+        instance = this;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            transform.position = hit.point;
+	}
+}
